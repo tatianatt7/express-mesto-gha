18abf5870ed4c37d52f653ebc946e816edeb37bc
Tatiana Petrova <tatianatt187@yandex.ru>
2023-08-15 05:25:59 +0300

    14ПР

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const auth = require('../middlewares/auth');
 
 const {
   createCard,
@@ -7,16 +9,49 @@ const {
   putLike,
   deleteLike,
 } = require('../controllers/cards');
-const { validateCardId } = require('../utils/validator');
 
-router.post('/', createCard);
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().pattern(/^(https?:\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:;/~+#-]*[\w@?^=%&/~+#-])?$/).required(),
+    }),
+  }),
+  createCard,
+);
 
 router.get('/', getCards);
+router.use(auth);
 
-router.delete('/:cardId', validateCardId, deleteCard);
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  deleteCard,
+);
 
-router.put('/:cardId/likes', validateCardId, putLike);
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  putLike,
+);
 
-router.delete('/:cardId/likes', validateCardId, deleteLike);
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().hex().length(24),
+    }),
+  }),
+  deleteLike,
+);
 
 module.exports = router;
